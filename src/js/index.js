3e004b7e30bcafcec124e3b590afd99426ff78cf
Altaf-dev <altaf090486@gmail.ru>
2024-06-24 16:47:06 +0500

    services

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,15 @@ const MENU_BTN = document.querySelector('.header__btn')
 const LOGO = document.querySelector('.header__logo')
 const MENU = document.querySelector('.header__menu')
 
+const MODAL_BG = document.querySelector('.modal-bg')
+const MODAL_THANKS = document.querySelector('.modal-thanks')
+const closeModalBtn = document.querySelectorAll('.closeModal')
+const openModal = document.querySelectorAll('.openModal')
+const modalItems = document.querySelectorAll('.modal')
+const MODAL_TITLE = document.querySelector('.modal-title')
+const MODAl_DESC = document.querySelector('.modal-desc')
+
+
 //Открытие мобильного меню
 MENU_BTN.addEventListener('click' , () => {
     MENU_BTN.classList.toggle('active')
@@ -17,3 +26,52 @@ $(window).resize(function () {
         MENU.classList.remove('active')
     }
 })
+
+//Остановка прокрутки сайта
+function scrollStop() {
+    document.querySelector("html").style.overflow = 'hidden'
+}
+
+//Восстановление прокрутки сайта
+function scrollAuto() {
+    document.querySelector("html").style.overflow = ''
+}
+
+//Функция закрытия модальных окон
+function closeModal() {
+    MODAL_BG.classList.remove('active')
+    modalItems.forEach((item) => {
+        item.classList.remove('active')
+    })
+    scrollAuto()
+}
+// //Модальное окно СПАСИБО
+// // function openThanksModal() {
+// //     closeModal()
+// //     MODAL_BG.classList.add('active')
+// //     MODAL_THANKS.classList.add('active')
+// //
+// // }
+//Закрытие модального окна по клику на крестик
+closeModalBtn.forEach((item) => {
+    item.addEventListener('click', () => {
+        closeModal()
+    })
+})
+//Открытие модального окна
+openModal.forEach((item) => {
+    item.addEventListener('click', () => {
+        let title = item.getAttribute('data-title')
+        let desc = item.getAttribute('data-desc')
+        modalItems.forEach((modals) => {
+            if (modals.classList.contains(item.getAttribute('data-modal'))) {
+                MODAL_BG.classList.add('active')
+                modals.classList.add('active')
+                MODAL_TITLE.innerText = title
+                MODAl_DESC.innerText = desc
+                scrollStop()
+            }
+        })
+    })
+})
+
